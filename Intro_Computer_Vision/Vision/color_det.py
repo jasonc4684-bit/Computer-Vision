ed2366c5470355f8d2a90aadf40e7593c30a2b2b
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 18:39:27 -0400

    initial layout for color detection

--- Intro_Computer_Vision/Vision/color_det.py
+++ Intro_Computer_Vision/Vision/color_det.py
@@ -1,13 +1,23 @@
 import cv2
+import cvzone
+from cvzone.ColorModule import ColorFinder
 
 vid = cv2.VideoCapture(0)
+
+myColorFinder = ColorFinder(trackBar=True)
+
+hsvVal = {}
 
 while True:
     success, img = vid.read()
 
     if not success:
         break
-    cv2.imshow("Video of falling tree", img)
+    cv2.imshow("Color detection", img)
+
+    imgColor, mask = myColorFinder.update(img, hsvVal)
+
+    imgStack = cvzone.stackImages(img, imgColor, mask, 3, 0.5)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
