418f42d3ae768566150a78336e839db8cf64b7b2
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 19:02:18 -0400

    comments and var.

--- Intro_Computer_Vision/Vision/blur.py
+++ Intro_Computer_Vision/Vision/blur.py
@@ -1,7 +1,13 @@
 import cv2
 
-img = cv2.imread("Intro_Computer_Vision/Intro/school.jpg")
+IMG = "Intro_Computer_Vision/Intro/school.jpg"
+
+img = cv2.imread(IMG)
+
+#higher () = more blury
 Blur = cv2.GaussianBlur(img, (15, 15), 0)
+
+#to gray
 cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("img", img)
 cv2.imshow("Blur img", Blur)
