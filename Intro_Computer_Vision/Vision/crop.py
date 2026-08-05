1b03eca1ceb519e54b29e3854ab55efb2f74365c
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 18:57:52 -0400

    added comments

--- Intro_Computer_Vision/Vision/crop.py
+++ Intro_Computer_Vision/Vision/crop.py
@@ -4,13 +4,14 @@ import cv2
 IMG = "Intro_Computer_Vision/Intro/school.jpg"
 img = cv2.imread(IMG)
 
-#         [y1:y2, x1:x2]
+#[y1:y2, x1:x2]
 y1 = 0
 y2 = 10
 x1=10
 x2=20
 crop = img[y1:y2, x1:x2]
 
+#show two img for contrast
 cv2.imshow("img", img)
 cv2.imshow("crop img", crop)
-cv2.waitKey(0)
+cv2.waitKey(0) # waiting until self-close the file
