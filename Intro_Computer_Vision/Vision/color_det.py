76316ede70ed85f1eb7e3cfb426e71d238ceb0ba
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 18:46:39 -0400

    "update on scale and testing HSV values"

--- Intro_Computer_Vision/Vision/color_det.py
+++ Intro_Computer_Vision/Vision/color_det.py
@@ -4,20 +4,21 @@ from cvzone.ColorModule import ColorFinder
 
 vid = cv2.VideoCapture(0)
 
-myColorFinder = ColorFinder(trackBar=True)
+myColorFinder = ColorFinder(trackBar=False)
 
-hsvVal = {}
+hsvVal = {'hmin': 108, 'smin': 67, 'vmin': 63, 'hmax': 179, 'smax': 255, 'vmax': 255}
 
 while True:
     success, img = vid.read()
 
     if not success:
         break
-    cv2.imshow("Color detection", img)
 
     imgColor, mask = myColorFinder.update(img, hsvVal)
 
-    imgStack = cvzone.stackImages(img, imgColor, mask, 3, 0.5)
+    imgStack = cvzone.stackImages([img, imgColor, mask], 3, 0.2)
+
+    cv2.imshow("Color detection", imgStack)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
