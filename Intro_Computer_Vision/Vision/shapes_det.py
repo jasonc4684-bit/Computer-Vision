a3f3cf38926243934ce3336fbb8db6132ac5927d
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 18:27:02 -0400

    update

--- Intro_Computer_Vision/Vision/shapes_det.py
+++ Intro_Computer_Vision/Vision/shapes_det.py
@@ -7,13 +7,13 @@ img = cv2.imread("Intro_Computer_Vision/Vision/img.jpg")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-grayCanny = cv2.Canny(imgGray, 150, 200)
+grayCanny = cv2.Canny(imgGray, 100, 250)
 
-gray_dilate = cv2.dilate(grayCanny, np.ones((6, 6), np.uint8), iterations=1)
+gray_dilate = cv2.dilate(grayCanny, np.ones((5, 5), np.uint8), iterations=2)
 
 gray_imgContours, conFound = findContours(img, gray_dilate, filter= None, drawCon= True)
 
-imgPresentSize = 0.4 # for most laptop, 
+imgPresentSize = 0.6 # for most laptop, 
 Columns = 3
 imgStack = cvzone.stackImages([img, imgGray, grayCanny, gray_dilate, gray_imgContours],
                                                         Columns, imgPresentSize)
