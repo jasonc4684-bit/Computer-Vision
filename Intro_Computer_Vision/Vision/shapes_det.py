2a2f7fdee7f01497eb8ecd3f61834ec7cdcc3f4b
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 19:59:59 -0400

    Refactor image processing variables in shapes_det.py
    Refactor variable names for clarity and improve readability.

--- Intro_Computer_Vision/Vision/shapes_det.py
+++ Intro_Computer_Vision/Vision/shapes_det.py
@@ -3,13 +3,17 @@ import cvzone
 import numpy as np
 from cvzone.Utils import findContours
 
-img = cv2.imread("Intro_Computer_Vision/Vision/img.jpg")
+Img = "Intro_Computer_Vision/Vision/img.jpg"
+img = cv2.imread(Img)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-grayCanny = cv2.Canny(imgGray, 100, 250)
+lower_bound = 100
+high_bound = 250
+grayCanny = cv2.Canny(imgGray, lower_bound, high_bound)
 
-gray_dilate = cv2.dilate(grayCanny, np.ones((5, 5), np.uint8), iterations=2)
+newArraySize = (5,5)
+gray_dilate = cv2.dilate(grayCanny, np.ones(newArraySize, np.uint8), iterations=2)
 
 gray_imgContours, conFound = findContours(img, gray_dilate, filter= None, drawCon= True)
 
