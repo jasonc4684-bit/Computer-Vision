956eb99861db745b0b2da6fe97ef9979bd973a08
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-05 19:55:56 -0400

    Refactor image loading and text display parameters
    Updated image path and added parameters for text display.

--- Intro_Computer_Vision/Vision/text.py
+++ Intro_Computer_Vision/Vision/text.py
@@ -1,9 +1,17 @@
 import cv2
 
-img = cv2.imread("../Computer_vision_essential/intro/school.jpg")
+Img = "Computer_vision/intro/school.jpg"
+img = cv2.imread(Img)
+
+#parameters for cv2.putText
+point = (10,20)
+cv2_Font = cv2.FONT_HERSHEY_DUPLEX
+font_thickness = 1
+textColor = (0,255,200)
+display_thickness = 2
 
 cv2.putText(img, "This is one high school at Virginia", 
-            (10,20), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,200), 2)
+            point, cv2_Font, thickness, textColor, display_thickness)
 
 cv2.imshow("img", img)
 
