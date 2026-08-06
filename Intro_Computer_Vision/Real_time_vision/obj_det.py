acbeb59de9133aebe381d07720a7f96299d5c6cf
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-06 16:06:26 -0400

    modules and setup for object det with YOLO

--- Intro_Computer_Vision/Real_time_vision/obj_det.py
+++ Intro_Computer_Vision/Real_time_vision/obj_det.py
@@ -1,1 +1,17 @@
 from ultralytics import YOLO
+import cv2
+import cvzone
+
+#capture local web cam, adjust webcam if needed
+cam = 0
+vid = cv2.VideoCapture(cam)
+
+while True:
+    success, img = vid.read()
+
+    if not success:
+        break
+    cv2.imshow("obj_det", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
