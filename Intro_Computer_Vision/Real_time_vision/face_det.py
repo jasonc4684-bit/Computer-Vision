591fd898d10ddfe403cca46012f56d8505e521fb
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-06 15:24:45 -0400

    added faceDector

---
+++ Intro_Computer_Vision/Real_time_vision/face_det.py
@@ -0,0 +1,19 @@
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+
+detector = FaceDetector()
+
+#capture local web cam, adjust webcam if needed
+cam = 0 # primary cam for laptop 
+vid = cv2.VideoCapture(cam)
+
+while True:
+    success, img = vid.read()
+
+    if not success:
+        break
+    cv2.imshow("Video of falling tree", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
