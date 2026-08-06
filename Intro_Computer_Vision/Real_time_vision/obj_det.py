fb542b3a9357f7154ca7f64910680d3fdd9d39dc
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-06 16:17:02 -0400

    model looping imgs

--- Intro_Computer_Vision/Real_time_vision/obj_det.py
+++ Intro_Computer_Vision/Real_time_vision/obj_det.py
@@ -6,8 +6,15 @@ import cvzone
 cam = 0
 vid = cv2.VideoCapture(cam)
 
+yoloModel = "yolo26m.pt" # change "s" if larger data is needed (ex."m")
+model = YOLO(yoloModel)
+
 while True:
     success, img = vid.read()
+    results = model(img, stream=True)
+    for result in results:
+        for box in result.bxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
 
     if not success:
         break
