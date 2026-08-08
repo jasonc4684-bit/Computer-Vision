72e0e816b40111b8869f17b761c94b5ccc78d069
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-07 21:08:48 -0400

    update

--- Intro_Computer_Vision/C_AI_vision/text_det.py
+++ Intro_Computer_Vision/C_AI_vision/text_det.py
@@ -2,9 +2,14 @@ from PIL import Image
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-printed")
-
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-printed")
 
 img = "AI_vision/img.png"
-result = pipe(img)
+image = Image.open(img).convert("RGB")
 
-print(result[0]["generated_text"])
+pixel_values = processor(images=image, return_tensors='pt').pixel_values
+generated_ids = model.generate(pixel_values)
+
+generatedText = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(generatedText)
