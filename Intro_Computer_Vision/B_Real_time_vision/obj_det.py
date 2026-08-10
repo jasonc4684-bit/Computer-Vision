3a318ae86fa7ea0ce02b04daaa6c0ca6af46d4c5
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-10 19:01:39 -0400

    rename trained files and update RPS

--- Intro_Computer_Vision/B_Real_time_vision/obj_det.py
+++ Intro_Computer_Vision/B_Real_time_vision/obj_det.py
@@ -6,7 +6,7 @@ import cvzone
 cam = 0
 vid = cv2.VideoCapture(cam)
 
-yoloModel = "yolo26m.pt" # change "s" if larger data is needed (ex."m")
+yoloModel = "rockPaperScissor.pt" # change "s" if larger data is needed (ex."m")
 model = YOLO(yoloModel)
 
 while True:
