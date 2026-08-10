8bd82b41280b502afb889221ceca8e47d43df1c3
Jason <jasonc4684@gmail.com>
2026-08-10 19:20:23 -0400

    rename

--- Intro_Computer_Vision/B_Real_time_vision/obj_det.py
+++ Intro_Computer_Vision/B_Real_time_vision/obj_det.py
@@ -6,7 +6,7 @@ import cvzone
 cam = 0
 vid = cv2.VideoCapture(cam)
 
-yoloModel = "rockPaperScissor.pt" # change "s" if larger data is needed (ex."m")
+yoloModel = "best.pt" # change "s" if larger data is needed (ex."m")
 model = YOLO(yoloModel)
 
 while True:
