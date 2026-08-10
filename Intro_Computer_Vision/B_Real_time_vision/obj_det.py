feecb5949f34f290b23d8ff2053b13ddfdcc338a
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-10 19:26:45 -0400

    readd best(1)

--- Intro_Computer_Vision/B_Real_time_vision/obj_det.py
+++ Intro_Computer_Vision/B_Real_time_vision/obj_det.py
@@ -6,7 +6,7 @@ import cvzone
 cam = 0
 vid = cv2.VideoCapture(cam)
 
-yoloModel = "best(1).pt" # change "s" if larger data is needed (ex."m")
+yoloModel = "best.pt" # change "s" if larger data is needed (ex."m")
 model = YOLO(yoloModel)
 
 while True:
