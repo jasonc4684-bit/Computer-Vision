599debf68642f18c18cb57efd3c92a0d7fe2b52b
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-20 14:41:06 -0400

    using torch.rand() in generating random tensors

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Creating tensor
-Matrix = torch.tensor([[[1,2,3],[4,5,6],[7,8,9]]])
-
-print(Matrix.shape)
+# Creating random tensors
+Matrix = torch.rand(3,3)
+print(Matrix)
