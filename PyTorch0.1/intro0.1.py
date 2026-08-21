0b9fe439529417ef849c8844b1c18279035e8081
Jason <jasonc4684@gmail.com>
2026-08-20 20:17:38 -0400

    creating tensor with zeros

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-# Creating random tensors
-imgHeight = 224
-imgWidth = 224
-Matrix = torch.rand(size=(3,imgHeight, imgWidth))
-print(Matrix.ndim, Matrix.shape)
+# Creating zeros tensors
+
+zeros = torch.zeros(3,4)
+print(zeros)
+
