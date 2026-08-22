66fdc1546fcf84d5a1f2bbaa56f52786e87d9b27
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-22 22:22:26 +0000

    update variables to match trasnpose output2

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -16,14 +16,13 @@ output = torch.tensor([[1,3,5],
                     requires_grad=False)      # checks gradients
 
 #3x2
-output2 = torch.tensor([[1,3],
-                        [2,4],
-                        [3,5]], 
+output2 = torch.tensor([[1,3,5],
+                        [2,4, 6]], 
                     dtype=torch.int16,      # tensor type
                     device = None,            # which device to use, ex. GPU
                     requires_grad=False)      # checks gradients
 
-matrix_mul = torch.mm(output, output2)
+matrix_mul = torch.mm(output, output2.T)
 #matrix multiplication, 
 # inner dimensions must be identical and 
 # shape will be the outer dimensions
@@ -31,6 +30,6 @@ matrix_mul = torch.mm(output, output2)
 #print(torch.mm(output, output2), output.shape, output2.shape) 
 print(f"Original shape for A is {output.shape}, and B is {output2.shape}")
 print(f"After the transpose, A stays the same {output.shape}, but B changed to {output2.T.shape}")
-print(f"The matrix multiplication output: {matrix_mul}\n")
+print(f"The matrix multiplication output: \n {matrix_mul}\n")
 print(matrix_mul.shape)
 
