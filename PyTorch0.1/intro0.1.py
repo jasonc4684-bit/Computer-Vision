f39d909e3f1bc496055428874375cc5e7cf7ae9b
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-22 22:27:01 +0000

    start matrix aggregation

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -4,32 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-# Adjusting tensor's dtype, 
-#dtype usuall caused 3 errors: incorrect datatype, shapes, and device
 
-#checks device 
-# 2x3
-output = torch.tensor([[1,3,5],
-                        [2,4,6]], 
-                    dtype=torch.int16,      # tensor type
-                    device = None,            # which device to use, ex. GPU
-                    requires_grad=False)      # checks gradients
+# using torch to manipulate/ find info about the data
 
-#3x2
-output2 = torch.tensor([[1,3,5],
-                        [2,4, 6]], 
-                    dtype=torch.int16,      # tensor type
-                    device = None,            # which device to use, ex. GPU
-                    requires_grad=False)      # checks gradients
-
-matrix_mul = torch.mm(output, output2.T)
-#matrix multiplication, 
-# inner dimensions must be identical and 
-# shape will be the outer dimensions
-
-#print(torch.mm(output, output2), output.shape, output2.shape) 
-print(f"Original shape for A is {output.shape}, and B is {output2.shape}")
-print(f"After the transpose, A stays the same {output.shape}, but B changed to {output2.T.shape}")
-print(f"The matrix multiplication output: \n {matrix_mul}\n")
-print(matrix_mul.shape)
-
+output = torch.arange(0, 100, 20)
+print(output)
