ca9042bca51ceb1726454cd13395765ccb6d6733
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-23 23:54:46 +0000

    added unsqueez pytorch code with example and demo

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -9,6 +9,11 @@ import random
 
 x = torch.tensor([[5,6,7]])
 print(f"X: {x}, and its shape is {x.shape}")
-squeezed_x = x.squeeze()
 
+squeezed_x = x.squeeze()
 print(f"with added squeeze(), x changed to {squeezed_x} and shape is {squeezed_x.shape}")
+
+y = torch.tensor([5,6,7])
+unsqueeze_y = y.unsqueeze(dim=0)
+print(f"y has tensor {y} with shape {y.shape}")
+print(f"with unsquezzed, {unsqueeze_y} with shape of {unsqueeze_y.shape}")
