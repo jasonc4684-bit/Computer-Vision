5a66118a1bc504648171a44e18a6c262de37ae5e
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-23 01:12:30 +0000

    add comments and stack operation to demonstrate tensor stacking

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -7,15 +7,23 @@ import random
 
 # using torch to manipulate/ find info about the data
 
+#just 1D from 1 to 20
 original = torch.arange(1., 21.)
 print(original)
 
+#reshape the tensor to 4x5
 reshape = original.reshape(4, 5)
-view = original.view(4,5)
 
+#similar to reshape in rearranging, but both share same memory
+view = original.view(4,5)
 print(reshape, reshape.shape)
 
+#changes both view and original data
 view[:, 0] = 30
 
 print(view, view.shape)
 print(original)
+
+#stack 3 original data to form 1x3, dim=1 allows the data on the same row to be identical
+stack = torch.stack([original, original, original], dim=0)
+print(stack)
