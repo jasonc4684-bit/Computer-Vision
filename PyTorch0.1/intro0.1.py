4ce41c78d18ecee54efce2545cece15709de9da9
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-23 23:44:32 +0000

    squeeze method

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -7,23 +7,7 @@ import random
 
 # using torch to manipulate/ find info about the data
 
-#just 1D from 1 to 20
-original = torch.arange(1., 21.)
-print(original)
+x = torch.tensor([[5,6,7]])
+print(x.shape)
 
-#reshape the tensor to 4x5
-reshape = original.reshape(4, 5)
-
-#similar to reshape in rearranging, but both share same memory
-view = original.view(4,5)
-print(reshape, reshape.shape)
-
-#changes both view and original data
-view[:, 0] = 30
-
-print(view, view.shape)
-print(original)
-
-#stack 3 original data to form 1x3, dim=1 allows the data on the same row to be identical
-stack = torch.stack([original, original, original], dim=0)
-print(stack)
+print(x.squeeze().shape)
