7a2368fb36615f216c95a62c48fe1d392412b2ec
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-24 20:56:09 +0000

    Accessing GPU startup with Colab

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -5,13 +5,5 @@ import matplotlib.pyplot as plt
 import random
 
 
-# reproducibility, also reduced randomness
-random_seed = 42
-torch.manual_seed(random_seed)
+# Accessing/ Running on GPU 
 
-
-tensor_1 = torch.rand(3,4)
-tensor_2 = torch.rand(3,4)
-
-print(f"first tensor {tensor_1}, second tensor {tensor_2}")
-print(tensor_1 == tensor_2)
