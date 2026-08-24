3bbc4635bab72329805100ac4f04984f29144fe0
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-24 21:52:33 +0000

    Refactor print statements for clarity and consistency in tensor operations

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -22,33 +22,33 @@ tensor.to(device) # prefer if cuda is available
 # extracurriculum 
 # random tensor with shape (7,7)
 tensor_1 = torch.rand(7,7)
-print(tensor_1.shape)
+print(f"tensor with shape (7,7) {tensor_1} shape : {tensor_1.shape}")
 
 # matrix multiplication from 2 with another with shape (1,7)
 tensor_2 = torch.rand(1,7)
 matmul_1_and_2 = torch.mm(tensor_1, tensor_2.T)
-print(matmul_1_and_2, matmul_1_and_2.shape)
+print(f"matrix multiplication from 2 with another with shape (1,7): {matmul_1_and_2} with shape {matmul_1_and_2.shape}")
 
 # using seed() to do excercise 1 and 2
 
 random_seed = 0
 torch.manual_seed(random_seed)
-tensor_1 = torch.rand(7,7)
-print(tensor_1.shape)
+tensor_3 = torch.rand(7,7)
+print(f"using seed() to do excercise 1 and 2 {tensor_3} with shape {tensor_3.shape}")
 
-tensor_2 = torch.rand(1,7)
-matmul_1_and_2 = torch.mm(tensor_1, tensor_2.T)
-print(matmul_1_and_2, matmul_1_and_2.shape)
+tensor_4 = torch.rand(1,7)
+matmul_3_and_4 = torch.mm(tensor_3, tensor_4.T)
+print(matmul_3_and_4, matmul_3_and_4.shape)
 
-print(sum(matmul_1_and_2), max(matmul_1_and_2), min(matmul_1_and_2), 
-        matmul_1_and_2.type(torch.float32).mean())
+print(f"sum of matmul_3_and_4: {sum(matmul_3_and_4)}, max: {max(matmul_3_and_4)}") 
+print(f"min of matmul_3_and_4: {min(matmul_3_and_4)}, mean: {matmul_3_and_4.type(torch.float32).mean()}")
 
-print(matmul_1_and_2.argmax(), matmul_1_and_2.argmin())
+print(f"max index of matmul_3_and_4: {matmul_3_and_4.argmax()}, min index: {matmul_3_and_4.argmin()}")
 
 torch.manual_seed(7)
 
-torch_3 = torch.rand(1,1,1,10)
-torch_4 = torch_3.squeeze()
-print(torch_3, torch_3.shape, torch_4, torch_4.shape)
+torch_5 = torch.rand(1,1,1,10)
+torch_6 = torch_5.squeeze()
+print(f"tensor with shape (1,1,1,10): {torch_5} shape : {torch_5.shape}, after squeezed, tensor: {torch_6} with shape {torch_6.shape}")
 
 
