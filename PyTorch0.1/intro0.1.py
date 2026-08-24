55f1d6f9ca8f2315f0b8ae6586317a3a1f37d93a
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-24 00:48:00 +0000

    from torch to numpy

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -7,11 +7,17 @@ import random
 
 # PyTorch & NumPy
 
-# utilize torch.from_numpy() in translating NumPy data to PyTorch for deep learning 
+# utilize torch.from_numpy() in translating NumPy data to PyTorch for deep learning , different memory
 # use torch.Tensor.numpy to translating data to NumPy
 
 np_data = np.arange(1, 10)
 print(np_data)
 
-to_torch = torch.from_numpy(np_data)
+to_torch = torch.from_numpy(np_data) # default dtype is int64 from numpy
 print(to_torch)
+
+torch_data = torch.arange(1,10)
+print(torch_data)
+
+to_np = torch.Tensor.numpy(to_torch)
+print(to_np)
