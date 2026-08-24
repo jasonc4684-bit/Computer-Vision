ec04cf412d12ceb34692c127252e5871156b56fd
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-24 21:24:24 +0000

    excercise 1

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -13,6 +13,16 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # if larger gpu is needed, use torch.cuda.device_count() to check for additional gpu
 
 tensor = torch.tensor([1,2,3])
-#tensor.to(device) if cuda is available
+tensor.to(device) # prefer if cuda is available
 
-print(tensor, tensor.device)
+#print(tensor, tensor.device)
+
+# to fix gpu error with numpy, use .cpu() to switch to cpu
+
+# extracurriculum 
+# random tensor with shape (7,7)
+tensor_1 = torch.rand(7,7)
+print(tensor_1.shape)
+
+# matrix multiplication from 2 with another with shape (1,7)
+print()
