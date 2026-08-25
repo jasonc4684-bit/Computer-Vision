410a55e63f7ff93fc978a5b3d58921f5839856e8
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-25 20:42:46 +0000

    linear regression from multi variables

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -10,3 +10,13 @@ from torch import nn # contains building block for pytorch's neural network
 
 # turns all data into numerical representation to the model (layers)
 # learns from the data's pattern and present output
+
+weight = 0.7
+bias = 0.3
+
+start = 0
+end = 1
+step == 0.02
+
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
