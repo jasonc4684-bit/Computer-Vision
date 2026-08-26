40101bc36eaa840b0517cd6da5bd8fd238c08006
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-26 00:15:20 +0000

    setup for using nn.Module in creating a model

--- PyTorch0.1/intro0.1.py
+++ PyTorch0.1/intro0.1.py
@@ -6,42 +6,7 @@ import random
 
 from torch import nn # contains building block for pytorch's neural network
 
-# Preparing and loading
+# building model 
 
-# turns all data into numerical representation to the model (layers)
-# learns from the data's pattern and present output
-
-weight = 0.7
-bias = 0.3
-
-start = 0
-end = 1
-step = 0.02
-
-X = torch.arange(start, end, step).unsqueeze(dim=1)
-y = weight * X + bias
-
-# data split
-training_set = int(0.8 * len(X))
-traing_X, training_y = X[:training_set], y[:training_set] 
-
-test_X, test_y = X[training_set:], y[training_set:] 
-
-print(len(traing_X), len(training_y), len(test_X), len(test_y))
-
-def plotdata(train_data=traing_X, 
-            train_label=training_y,
-            test_data=test_X,
-            test_label=test_y,
-            prediction=None):
-    plt.figure(figsize=(10,7))
-
-    plt.scatter(traing_X, training_y, s=4, c='b', label='Training data')
-
-    plt.scatter(test_X, test_y, s=4, c='r', label='Testing data')
-
-    if prediction:
-        plt.scatter(test_X, prediction, s=4, c='g', label="Prediction")
-
-    plt.legend(prop={"size":14})
-
+class LinearRegModel(nn.Module): # inherit nn.Module for useful prebuilt tools
+    def __init__(self):
