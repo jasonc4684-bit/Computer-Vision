0e18960339faf5093745a2e160f2dce1d9ff8681
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-26 00:49:28 +0000

    comments on the building block for subclass class

--- PyTorch0.1/lesson.py
+++ PyTorch0.1/lesson.py
@@ -12,14 +12,19 @@ class LinearRegModel(nn.Module): # inherit nn.Module for useful prebuilt tools
     def __init__(self):
         super().__init__() # access all methods from nn.Module
 
+        #Initializing 
+
         #nn.Paramter auto assign and train the left_side to deep learning afterward
-        self.weights = nn.Parameter(torch.randn(1,
+        self.weights = nn.Parameter(torch.randn(1,      # starts off a random value
                                                 requires_grad=True, #optimizes the loss function
-                                                dtype=torch.float))
+                                                dtype=torch.float))  #default to float32
 
         self.bias = nn.Parameter(torch.randn(1,
                                                 requires_grad=True,
                                                 dtype=torch.float))
     
+    #Required func for pytorch deeplearning, to override
     def forward(self, x:torch.Tensor) -> torch.Tensor: #expects input x to be same with output, torch.Tensor
         return self.weights * x + self.bias         # linear regression 
+
+        
