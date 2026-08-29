4121c3b194f0e72519a5186865a874984267bf81
jasonc4684-bit <jasonc4684@gmail.com>
2026-08-29 15:00:57 +0000

    refactor training loop: clean up comments and remove unnecessary line

--- PyTorch0.1/lesson.py
+++ PyTorch0.1/lesson.py
@@ -39,7 +39,6 @@ class LinearRegModel(nn.Module):  # inherit nn.Module for useful prebuilt tools
         super().__init__()  # access all methods from nn.Module
 
         # Initializing
-
         # nn.Paramter auto assign and train the left_side to deep learning afterward
         self.weights = nn.Parameter(
             torch.randn(
@@ -57,7 +56,6 @@ class LinearRegModel(nn.Module):  # inherit nn.Module for useful prebuilt tools
     ) -> torch.Tensor:  # expects input x to be same with output, torch.Tensor
         return self.weights * x + self.bias  # linear regression
 
-
 # torch.optim() optimizes/help with gradient descent
 # torch.utils.data.Dataset() map the key and sample pair
 # torch.utils.data.Dataloader() iterate over the torch Dataset
@@ -66,7 +64,6 @@ class LinearRegModel(nn.Module):  # inherit nn.Module for useful prebuilt tools
 torch.manual_seed(42)
 model_0 = LinearRegModel()
 print(list(model_0.parameters()), model_0.state_dict())
-
 
 def plotdata(
     train_data=training_X,
@@ -99,15 +96,14 @@ plotdata(
 
 # loss/cost/criteria function measures the distance apart is the prediction
 # ex. nn.L1Loss() uses MAE(mean abs. error) between x and y, which nn.MSELoss uses mean^2
-# nn.L1Loss() = torch.mean(abs(y_predic - y_test))
 
+# nn.L1Loss() = torch.mean(abs(y_predic - y_test))
 loss_fn = nn.L1Loss()
 
 # optimizers - account the loss and adjust the model's parameters
-
-optim_fn = torch.optim.SGD(
-    params=model_0.parameters(), lr=0.01  # stochastic gradient descent optimizer
-)  # learning rate, propotion to change in parameters
+optim_fn = torch.optim.SGD( # stochastic gradient descent optimizer
+    params=model_0.parameters(), lr=0.01   # learning rate, propotion to change in parameters
+) 
 
 # epochs, loops
 epochs = 200
@@ -118,7 +114,6 @@ test_loss_list = []
 
 for epoch in range(epochs):
     model_0.train()  # auto enables required-gradients
-
     # 1. forward pass
     y_pred = model_0(training_X)
 
@@ -136,7 +131,7 @@ for epoch in range(epochs):
 
     model_0.eval()  # disables required-gradients
     with torch.inference_mode():
-        # forward pass
+        # forward pass 
         y_pred = model_0(test_X)
         # loss
         loss_test = loss_fn(y_pred, test_y.type(torch.float))
@@ -149,9 +144,9 @@ for epoch in range(epochs):
 
         print(f"epoch {epoch}, loss {loss}, test loss {loss_test}")
 
-        print(model_0.state_dict())
+        print(model_0.state_dict()) #print the parameters' values
 
-plotdata(prediction=y_pred)
+plotdata(prediction=y_pred) 
 
 plt.figure(figsize=(10, 7))
 plt.plot(epoch_list, loss_list, label="loss from epoch")
